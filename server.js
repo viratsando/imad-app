629a82f8075188ae652090c8882b546035266177
viratsando <30686147+viratsando@users.noreply.github.com>
2017-08-05 19:46:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res) {
+    res.send('mutta parotta');
+});
+
+app.get('/article-two', function(req, res) {
+    res.send('idli');
+});
+
+app.get('/article-three', function(req, res) {
+    res.send('hello this is Santhosh i am an app developer and also an programmer and also an entrepernuer');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
